2ce4a261388d81a3b206f50f747da4658d49bf68
ucok <ulubis98@gmail.com>
2022-09-07 21:09:51 +0700

    add variance function

--- src/main/kotlin/linReg.kt
+++ src/main/kotlin/linReg.kt
@@ -1,3 +1,7 @@
 fun mean(values: List<Int>): Int {
     return values.sum() / values.size
 }
+
+fun variance(values: List<Int>, mean_val: Int) : Int {
+    return values.sumOf { x -> (x - mean_val) * (x - mean_val) }
+}
