77e6d1359ab827d94f5422d7806ebaceeaa0260b
ucok <ulubis98@gmail.com>
2022-09-07 21:18:10 +0700

    add covariance

--- src/main/kotlin/linReg.kt
+++ src/main/kotlin/linReg.kt
@@ -6,8 +6,17 @@ fun variance(values: List<Int>, mean_val: Int) : Int {
     return values.sumOf { x -> (x - mean_val) * (x - mean_val) }
 }
 
+fun covariance(x: List<Int>, mean_x: Int, y: List<Int>, mean_y: Int): Float {
+    var covar = 0.0F
+    for (i in 0..x.size) {
+        covar += (x[i] - mean_x) * (y[i] - mean_y)
+    }
+    return covar
+}
+
 fun main() {
     val d = listOf(1, 4, 2, 3, 5)
     println(mean(d))
     println(variance(d, mean(d)))
+    covariance(d, 3, d, 2)
 }
