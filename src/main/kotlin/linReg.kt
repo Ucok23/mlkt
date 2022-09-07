d21dce0a14dc867482414ff0cfc6fed04cc10e9e
ucok <ulubis98@gmail.com>
2022-09-07 21:10:15 +0700

    add main function

--- src/main/kotlin/linReg.kt
+++ src/main/kotlin/linReg.kt
@@ -5,3 +5,9 @@ fun mean(values: List<Int>): Int {
 fun variance(values: List<Int>, mean_val: Int) : Int {
     return values.sumOf { x -> (x - mean_val) * (x - mean_val) }
 }
+
+fun main() {
+    val d = listOf(1, 4, 2, 3, 5)
+    println(mean(d))
+    println(variance(d, mean(d)))
+}
